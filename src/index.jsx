bc5d027e13d132309c38d967320122810b06d738
Andrew Stegmaier <ansteg@microsoft.com>
2019-12-28 13:26:36 -0800

    Modify parent component to clarify that it won't work.

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <div>
             <h1>Hello Fast Refresh!</h1>
-            <p>Count {count}. It preserves state!</p>
+            <p>Count {count}. It should preserve state, but it doesn't!</p>
             <button onClick={() => setCount(c => c + 1)}>Increment</button>
             <ChildComponent />
         </div>
